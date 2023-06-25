9db31ce1722c3346377ab0612cd1daa6d1aaadaa
martin-karani <karanimartinmk@gmail.com>
2023-06-25 05:30:59 +0300

    feat: add network service

--- client/src/index.js
+++ client/src/index.js
@@ -3,22 +3,21 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import NetworkService from "./network-service";
 import "./index.css";
 import configureStore from "./store/configureStore";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const store = configureStore();
 
+NetworkService.setupInterceptors(store);
 root.render(
-    <React.StrictMode store={store}>
-        <Provider>
-            <App />
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
