fce6c2cc0f4a73e4a17c3890178aa4e5b2f3e65e
martin-karani <karanimartinmk@gmail.com>
2023-06-25 05:22:31 +0300

    feat: add auth context

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,7 +1,169 @@
-import { useContext } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 
-import AuthContext from "../contexts/AuthContext";
+import jwtDecode from "jwt-decode";
 
-const useAuth = () => useContext(AuthContext);
+import SplashScreen from "../components/SlashScreen";
+import authHeader from "../services/auth-header";
+import { API_BASE } from "../utils/API_BASE";
+import axios from "../utils/axios";
 
-export default useAuth;
+const initialAuthState = {
+    isAuthenticated: false,
+    isInitialised: false,
+    user: null,
+    lastVisitedPatient: null,
+};
+
+const isValidToken = (accessToken) => {
+    if (!accessToken) {
+        return false;
+    }
+
+    const decoded = jwtDecode(accessToken);
+    const currentTime = Date.now() / 1000;
+    return decoded.exp > currentTime;
+};
+
+const setSession = (accessToken) => {
+    if (accessToken) {
+        localStorage.setItem("accessToken", accessToken);
+        axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+    } else {
+        localStorage.removeItem("accessToken");
+        delete axios.defaults.headers.common.Authorization;
+    }
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "INITIALISE": {
+            const { isAuthenticated, user } = action.payload;
+            return {
+                ...state,
+                isAuthenticated,
+                isInitialised: true,
+                user,
+            };
+        }
+        case "LOGIN": {
+            const { user } = action.payload;
+            return {
+                ...state,
+                isAuthenticated: true,
+                user,
+            };
+        }
+        case "LOGOUT": {
+            return {
+                ...state,
+                isAuthenticated: false,
+                user: null,
+            };
+        }
+        default: {
+            return { ...state };
+        }
+    }
+};
+
+const AuthContext = createContext({
+    ...initialAuthState,
+    method: "JWT",
+    login: () => Promise.resolve(),
+    logout: () => {},
+});
+
+export const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialAuthState);
+
+    const login = async (email, password) => {
+        const response = await axios.post(`${API_BASE}/auth/login`, {
+            email,
+            password,
+        });
+        const { accessToken, user } = response.data.data;
+        setSession(accessToken);
+        const patientId = "";
+
+        dispatch({
+            type: "LOGIN",
+            payload: {
+                user,
+            },
+        });
+    };
+
+    const logout = () => {
+        localStorage.clear();
+        setSession(null);
+        dispatch({ type: "LOGOUT" });
+
+        // removeCookie("last_viewed_patient_id");
+    };
+
+    useEffect(() => {
+        const initialise = async () => {
+            try {
+                const accessToken = window.localStorage.getItem("accessToken");
+                if (accessToken && isValidToken(accessToken)) {
+                    setSession(accessToken);
+                    const decoded = jwtDecode(accessToken);
+
+                    let fetchURL = `${API_BASE}/auth/user`;
+
+                    const response = await axios.get(fetchURL, {
+                        headers: authHeader(),
+                    });
+                    const { user } = response.data.data;
+
+                    dispatch({
+                        type: "INITIALISE",
+                        payload: {
+                            ...state,
+                            isAuthenticated: true,
+                            user,
+                        },
+                    });
+                } else {
+                    dispatch({
+                        type: "INITIALISE",
+                        payload: {
+                            isAuthenticated: false,
+                            user: null,
+                        },
+                    });
+                }
+            } catch (err) {
+                dispatch({
+                    type: "INITIALISE",
+                    payload: {
+                        isAuthenticated: false,
+                        user: null,
+                    },
+                });
+            }
+        };
+
+        initialise();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    if (!state.isInitialised) {
+        return <SplashScreen />;
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                ...state,
+                method: "JWT",
+                login,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
