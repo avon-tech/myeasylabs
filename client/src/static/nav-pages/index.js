40ac71ce725c7079b7a60b1c9f433d71c84f743f
martin-karani <karanimartinmk@gmail.com>
2023-07-01 16:22:40 +0300

    feat: add catalog icon

--- client/src/static/nav-pages/index.js
+++ client/src/static/nav-pages/index.js
@@ -1,30 +1,37 @@
-import HomeIcon from '@mui/icons-material/Home';
-import PersonIcon from '@mui/icons-material/PersonOutlined';
-import LocationCityIcon from '@mui/icons-material/LocationCity';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/PersonOutlined";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import ListAltIcon from "@mui/icons-material/ListAlt";
 
 export const client_pages = [
     {
         id: 1,
         title: "Dashboard",
         href: "/dashboard",
-        icon: <HomeIcon style={{color:"#FFF",fontSize:"20px"}}/>,
+        icon: <HomeIcon style={{ color: "#FFF", fontSize: "20px" }} />,
     },
     {
         id: 2,
-        icon: <PersonIcon style={{color:"#FFF",fontSize:"20px"}} />,
+        icon: <PersonIcon style={{ color: "#FFF", fontSize: "20px" }} />,
         title: "Myself",
         href: "/myself",
     },
     {
         id: 3,
-        icon: <LocationCityIcon style={{color:"#FFF",fontSize:"20px"}} />,
+        icon: <LocationCityIcon style={{ color: "#FFF", fontSize: "20px" }} />,
         title: "Client",
         href: "/client-profile",
     },
     {
-        id: 6,
-        icon: <ExitToAppIcon style={{color:"#FFF",fontSize:"20px"}} />,
+        id: 4,
+        icon: <ListAltIcon style={{ color: "#FFF", fontSize: "20px" }} />,
+        title: "Catalog",
+        href: "/catalog",
+    },
+    {
+        id: 5,
+        icon: <ExitToAppIcon style={{ color: "#FFF", fontSize: "20px" }} />,
         title: "Logout",
         href: "/",
         logout: true,
