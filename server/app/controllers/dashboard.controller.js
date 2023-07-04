c4c2f6c6527c79336e9eb83728eb4f4e98cde405
davidjhp <david10@davidjpotter.com>
2023-07-04 16:47:20 -0700

    .

--- server/app/controllers/dashboard.controller.js
+++ server/app/controllers/dashboard.controller.js
@@ -16,10 +16,11 @@ const search = async (req, res) => {
         };
         let params = [];
         $sql = `
-        select p.firstname, p.lastname, o.status, o.created, o.updated
+        select p.firstname, p.lastname, s.name status, o.created, o.updated
         from orders o
         left join patient p on p.id = o.patient_id
         left join users u on u.id = o.updated_user_id
+		left join status s on s.id = o.status
         where o.client_id = $1
         `;
         params.push(req.client_id);
@@ -48,7 +49,7 @@ const search = async (req, res) => {
             return {
                 firstname: row.firstname,
                 lastname: row.lastname,
-                status: translationMap[row.status.trim()],
+                status: row.status,
                 created: moment(row.updated).format("MMM D, YYYY"),
                 updated: moment(row.updated).format("MMM D, YYYY"),
             };
