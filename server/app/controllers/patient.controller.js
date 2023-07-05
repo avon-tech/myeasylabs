c31dab3cd10455f4585523a713a59e0b77ffbbd5
martin-karani <karanimartinmk@gmail.com>
2023-07-05 16:56:45 +0300

    fix: patient query to match the exact term in the search

--- server/app/controllers/patient.controller.js
+++ server/app/controllers/patient.controller.js
@@ -8,9 +8,9 @@ const searchPatientsByClientId = async (req, res) => {
             `select id, firstname, lastname
             from patient
             where client_id = $1
-              and (lower(firstname) like '%' || lower($2) || '%' 
-                or lower(lastname) like '%' || lower($2) || '%'
-                or lower(email) like '%' || lower($2) || '%')`,
+              and (lower(firstname) like '%$2%' 
+                or lower(lastname) like '%$2%'
+                or lower(email) like '%$2%'`,
             [req.client_id, term]
         );
         successMessage.data = patients.rows;
