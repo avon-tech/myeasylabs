ac4898f9335fb688ac1e43e152cedf1d795cdc7b
martin-karani <karanimartinmk@gmail.com>
2023-07-08 00:46:34 +0300

    feat: complete the scheduled job hourly.js

--- server/app/jobs/hourly.js
+++ server/app/jobs/hourly.js
@@ -2,14 +2,17 @@
 David
 July 4 2023
 This job sends an email to patients to purchase lab tests
-Test the job from command line: "node ./server/jobs/hourly.js"
+Test the job from command line: "node ./server/app/jobs/hourly.js"
 */
-
 const nodeCron = require("node-cron");
+const jwt = require("jsonwebtoken");
 
-const job = nodeCron.schedule("0 * * * * *", () => {
+const db = require("../db");
+const { transporter, sgMail } = require("../routes/password-reset.routes");
 
-    sql = `
+const job = nodeCron.schedule("0 * * * * *", async () => {
+    try {
+        sql = `
         select o.id order_id
             , o.created
             , p.email
@@ -26,25 +29,55 @@ const job = nodeCron.schedule("0 * * * * *", () => {
         where o.status = 'STP' /*yes there is an index on o.status*/
         and ponl.dt is null /*was not sent today*/
         and current_date - o.created::date in (0,1,2,4,7,14,21,30)
-        `
+        `;
 
-    //send sql to database
+        //send sql to database
+        const result = await db.query(sql);
 
-    //for each row returned:
+        result.rows.forEach(async (row) => {
+            const { order_id, email, firstname, provider_name } = row;
+            function encryptOrderId(order_id) {
+                const secretKey = process.env.JWT_SECRET;
+                return jwt.sign({ order_id }, secretKey);
+            }
+            // Generate the URL with the encrypted order_id
+            const url = `https://app.myeasylabs.com/patient-purchase?oid=${encryptOrderId(
+                order_id
+            )}`;
 
-        //send the following email
+            const labTestTemplate = (email, firstname, provider_name, url) => {
+                const from = process.env.EMAIL_LOGIN;
+                const to = email;
+                const subject = "Lab Test";
+                const html = `
+                Hi ${firstname},
+                ${provider_name} has ordered lab testing for you.
+                Click here ${url} to purchase the tests.
+              `;
+
+                return { from, to, subject, html };
+            };
+
+            const emailTemplate = labTestTemplate(
+                email,
+                firstname,
+                provider_name,
+                url
+            );
+            if (process.env.NODE_ENV === "production") {
+                await sgMail.send(emailTemplate);
+            } else {
+                await transporter.sendMail(emailTemplate);
+            }
+
+            notifyQuery =
+                "insert into patient_order_notify_log values ($1, current_date)";
+            await db.query(notifyQuery, [order_id]);
+        });
 
         //url = "https://app.myeasylabs.com/patient-purchase?oid=$oid"
-            //$oid is the order_id encrypted.  make it at least 64 characters.
-
-        body = `
-            Hi $firstname,
-            $provider_name has ordered lab testing for you.
-            Click here {$url} to purchase the tests.
-        `
-
-        //send email
-        
-        sql = "insert into patient_order_notify_log values ($order_id, current_date)"
-
+        //$oid is the order_id encrypted.  make it at least 64 characters.
+    } catch (error) {
+        console.error("Error in cron job:", error);
+    }
 });
